ad45f93d14eb214dd810e03c62e17ada37e52ebb
jungsunbeen <tjsqls041103@gmail.com>
2025-07-14 11:01:09 +0900

    refactor: improve formatting of TheFooter component

--- src/components/layout/TheFooter.tsx
+++ src/components/layout/TheFooter.tsx
@@ -3,8 +3,10 @@ export function TheFooter() {
     <>
     
     <footer className="w-full max-w-[600px] mx-auto py-4 text-center text-sm text-gray-500 bg-gray-100">
-        <div className="text-lg" >선빈 X 인영</div>
-      © {new Date().getFullYear()} CAU LIKELION All rights reserved.
+      <div className="text-lg pb-4">선빈 X 인영</div>
+      <div>
+        © {new Date().getFullYear()} CAU LIKELION All rights reserved.
+      </div>
     </footer>
     </>
   );
